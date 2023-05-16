3c2c487393222329824a079c715af40148938551
Mohammad Amin Dadgar <dadgaramin96@gmail.com>
2023-05-16 15:30:38 +0330

    Update the example!

--- example/sender_TC.py
+++ example/sender_TC.py
@@ -7,10 +7,12 @@ if __name__ == "__main__":
     broker_url = "localhost"
     rabbit_mq = RabbitMQ(broker_url=broker_url)
 
-    rabbit_mq.connect(Queue.DISCORD_ANALYZER)
+    rabbit_mq.connect(Queue.DISCORD_BOT)
 
     content = {"content": "content_analyzer_publish"}
 
     rabbit_mq.publish(
-        Queue.DISCORD_ANALYZER, event=Event.DISCORD_ANALYZER.RUN, content=content
+        Queue.DISCORD_ANALYZER, 
+        event=Event.DISCORD_ANALYZER.RUN, 
+        content=content
     )

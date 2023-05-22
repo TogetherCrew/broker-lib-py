d9c1dc7ee7f16f50d3fcfeeb322aa12620195542
Mohammad Amin Dadgar <dadgaramin96@gmail.com>
2023-05-22 15:13:35 +0330

    Removing unused codes!

--- tc_messageBroker/rabbit_mq/saga/choreography_base.py
+++ tc_messageBroker/rabbit_mq/saga/choreography_base.py
@@ -5,8 +5,4 @@ class IChoreography:
     def __init__(self, name: str, transactions: list[ITransaction]) -> None:
         self.name = name
         self.transactions = transactions
-
-
-# class IChoreographyDict:
-#     def __init__(self, choreography_dict: dict[str, IChoreography]) -> None:
-#         self.choreography_dict = choreography_dict
+        

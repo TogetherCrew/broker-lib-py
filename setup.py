71e5dbf6c19ce923ce300247a036869fc27eebf0
Mohammad Amin <dadgaramin96@gmail.com>
2023-07-03 13:59:30 +0330

    Updated declare_queue default values! - Now setting the declare queue default as durable=True and auto_delete=False

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="tc-messageBroker",
-    version="1.3.0",
+    version="1.4.0",
     author="Mohammad Amin Dadgar, RnDAO",
     maintainer="Mohammad Amin Dadgar",
     maintainer_email="dadgaramin96@gmail.com",

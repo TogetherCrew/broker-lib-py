31a5a43cc49c606e84dd88cc248859fda362d179
Mohammad Amin <dadgaramin96@gmail.com>
2023-09-15 09:24:48 +0330

    library version increase!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="tc-messageBroker",
-    version="1.5.2",
+    version="1.5.3",
     author="Mohammad Amin Dadgar, RnDAO",
     maintainer="Mohammad Amin Dadgar",
     maintainer_email="dadgaramin96@gmail.com",

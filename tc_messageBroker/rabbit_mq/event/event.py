84e3344b5761fe425184ad3dc376ce98f2213853
Mohammad Amin <dadgaramin96@gmail.com>
2024-10-16 09:38:50 +0330

    feat: added new events!

--- tc_messageBroker/rabbit_mq/event/event.py
+++ tc_messageBroker/rabbit_mq/event/event.py
@@ -1,6 +1,7 @@
 from .events_microservice import (
     DiscordAnalyzerEvent,
     DiscordBotEvent,
+    DiscordHivemindAdapterEvent,
     HivemindEvent,
     ServerEvent,
     TwitterAnalyzerEvent,
@@ -12,6 +13,7 @@ class Event:
     SERVER_API = ServerEvent
     DISCORD_BOT = DiscordBotEvent
     DISCORD_ANALYZER = DiscordAnalyzerEvent
+    DISCORD_HIVEMIND_ADAPTER = DiscordHivemindAdapterEvent
     TWITTER_BOT = TwitterBotEvent
     TWITTER_ANALYZER = TwitterAnalyzerEvent
     HIVEMIND = HivemindEvent
